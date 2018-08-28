f8b101da94595a7357b00a8240f0551f131861ac
Parveen Kaur <Parveen Kaur@zorang>
2018-08-28 18:36:16 +0530

    3rd commit

--- SimpleDemoProject/src/com/java/helloworld/HelloWorld.java
+++ SimpleDemoProject/src/com/java/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.java.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World India");
 	}
 
 }
